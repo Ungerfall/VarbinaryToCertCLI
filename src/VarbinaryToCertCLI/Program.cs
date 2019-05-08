2fec029a831c6793bf49500364a4e55fe40a480b
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-05-08 18:48:46 +0400

    Add hexPart processing of hexed password

--- src/VarbinaryToCertCLI/Program.cs
+++ src/VarbinaryToCertCLI/Program.cs
@@ -19,30 +19,29 @@ namespace VarbinaryToCertCLI
 
 			var arg = args[0];
 			var byteList = new List<byte>();
-			var hexPart = arg.Substring(2);
-			for (int i = 0; i < hexPart.Length / 2; i++)
-			{
-				var hexNumber = hexPart.Substring(i * 2, 2);
-				byteList.Add((byte) Convert.ToInt32(hexNumber, 16));
-			}
-
 			try
 			{
+				var hexPart = arg.Substring(2);
+				for (int i = 0; i < hexPart.Length / 2; i++)
+				{
+					var hexNumber = hexPart.Substring(i * 2, 2);
+					byteList.Add((byte) Convert.ToInt32(hexNumber, 16));
+				}
+
 				using (var ms = new MemoryStream(byteList.ToArray()))
 				using (var fileStream = File.Create("certificate.pfx"))
 				{
 					ms.WriteTo(fileStream);
 				}
+
+				Console.WriteLine("Varbinary certificate has been successfully imported to certificate.pfx file"
+					+ $"{Environment.NewLine}Press any key to exit...");
+				Console.ReadKey();
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex.Message);
+				Console.WriteLine($"Import failed: {ex.Message}");
 			}
-
-			Console.WriteLine("Varbinary certificate has been successfully imported to certificate.pfx file"
-				+ $"{Environment.NewLine}Press any key to exit...");
-			Console.ReadKey();
 		}
-
 	}
 }
